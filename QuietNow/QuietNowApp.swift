0f3e6c27108241ffda83a462e2545db1d7352ff0
Nalani <92067880+NalaniFlynns@users.noreply.github.com>
2026-06-20 01:10:01 +0800

    Update QuietNowApp.swift

--- QuietNow/QuietNowApp.swift
+++ QuietNow/QuietNowApp.swift
@@ -2,30 +2,18 @@
 //  QuietNowApp.swift
 //  QuietNow
 //
-//  Created by Spotlight Deveaux on 2023-03-23.
-//
 
 import SwiftUI
 
 @main
 struct QuietNowApp: App {
     var body: some Scene {
-        DocumentGroup(viewing: TrackDocument.self) { loadedFile in
-            TrackDocumentView(file: loadedFile.document)
-            #if os(macOS)
-                // Under macOS, SwiftUI allows a very interesting default layout without a minimum set.
-                .frame(minWidth: 500.0, minHeight: 500.0)
-            #endif
+        // 废弃原作者的 DocumentGroup，改用标准的 WindowGroup
+        WindowGroup {
+            MainView() // 加载我们自己写的主界面
         }
+        
         #if os(macOS)
-        // Attempt to avoid having a save button.
-        .commands {
-            CommandGroup(replacing: .saveItem) {}
-        }
-        #endif
-
-        #if os(macOS)
-            // Allow configuring model path
             Settings {
                 SettingsView()
             }
